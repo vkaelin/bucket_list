6ba9c2e79aafeca6415c28a8e957515528e3dbb8
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-22 17:46:04 +0200

    Updating a record from a form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -16,4 +16,18 @@ class IdeasController < ApplicationController
     idea.save!
     redirect_to(ideas_index_path)
   end
+
+  def edit
+    id = params[:id]
+    @idea = Idea.find(id)
+  end
+
+  def update
+    idea = Idea.find(params[:id])
+    idea.title = params[:title]
+    idea.done_count = params[:done_count]
+    idea.photo_url = params[:photo_url]
+    idea.save!
+    redirect_to(account_ideas_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,10 @@ Rails.application.routes.draw do
 
   post '/ideas/create'
 
+  get   '/ideas/:id/edit', to: 'ideas#edit', as: 'edit_idea'
+  
+  patch '/ideas/:id',      to: 'ideas#update', as: 'idea'
+
   get '/account/ideas'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
