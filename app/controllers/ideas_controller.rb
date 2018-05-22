f59a87fc0364a169dab1bfe3da35b5dd7f27e00c
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-22 13:39:52 +0200

    Posting a form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,4 +3,11 @@ class IdeasController < ApplicationController
     @search_term = params[:q]
     logger.info("Search completed using #{@search_term}.")
   end
+
+  def new
+  end
+
+  def create
+    redirect_to(ideas_index_path)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  
+
   get 'home/index'
 
   get 'styles/atoms'
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get 'styles/organisms'
 
   get 'ideas/index'
+
+  get  '/ideas/new'
+  
+  post '/ideas/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
