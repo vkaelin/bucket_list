f0440c65810b183004166f909713fffa1a83ebda
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-23 13:45:54 +0200

    Moved icons to public folder and fixed tests

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -1,13 +1,13 @@
 require "application_system_test_case"
 
 class SimpleNavigationsTest < ApplicationSystemTestCase
-  # test 'search term is displayed' do
-  #   visit('/')
-  #   assert page.has_content?('Bucket List')
-  #   fill_in('q', with: 'Spain').send_keys(:enter)
-  #   assert has_content?('Spain')
-  #   assert current_url.include?('q=Spain')
-  # end
+  test 'search term is displayed' do
+    visit('/')
+    assert page.has_content?('Bucket List')
+    fill_in('q', with: 'Spain').send_keys(:enter)
+    assert has_content?('Spain')
+    assert current_url.include?('q=Spain')
+  end
 
   # test 'style guide navigation' do NOT THE SAME AS THE ONE IN VIDEO
   #   visit('/')
@@ -23,18 +23,18 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
   #   assert page.has_content?('My ideas')
   # end
 
-  # test 'style guide navigation' do
-  #   visit('/')
-  #   click_on('Styleguide')
-  #   assert page.has_content?('Atoms')
-  #   assert page.has_content?('Molecules')
-  #   assert page.has_content?('Organisms')
-  #   click_on('Molecules')
-  #   assert page.has_content?('Card')
-  #   click_on('Organisms', match: :first)
-  #   assert page.has_content?('Create goal')
-  #   click_on('My Bucket List', match: :first)
-  #   assert page.has_content?('Some ideas')
-  # end
+  test 'style guide navigation' do
+    visit('/')
+    click_on('Style Guide')
+    assert page.has_content?('Atoms')
+    assert page.has_content?('Molecules')
+    assert page.has_content?('Organisms')
+    click_on('Molecules')
+    assert page.has_content?('Card')
+    click_on('Organisms', match: :first)
+    assert page.has_content?('Create goal')
+    click_on('My Bucket List', match: :first)
+    assert page.has_content?('Some ideas')
+  end
 
 end
