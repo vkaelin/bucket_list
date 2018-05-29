c5d31947cb52bff662d94c31b2e950b17e995b7d
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-29 14:16:41 +0200

    Customized menu

--- config/routes.rb
+++ config/routes.rb
@@ -21,5 +21,9 @@ Rails.application.routes.draw do
 
   get '/account/ideas'
 
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
