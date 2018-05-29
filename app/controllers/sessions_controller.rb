9748f1d11dd3b31d97b8ccea3980a808abb023f2
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-29 12:13:09 +0200

    Improved conditional for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
 
-    if(user.present?)
-      if(user.authenticate(params[:password]))
-        session[:user_id] = user.id
-        redirect_to(account_ideas_path)
-      else
-        flash[:alert] = "Email or password were invalid. Please try again"
-        render 'new'
-      end
+    if(user.present? && user.authenticate(params[:password]))
+      session[:user_id] = user.id
+      redirect_to(account_ideas_path)
     else
       flash[:alert] = "Email or password were invalid. Please try again"
       render 'new'
