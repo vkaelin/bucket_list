70fdcaa9acedf28b75d5213a045b72229d231e7a
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-30 14:14:23 +0200

    More super user abilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :ensure_admin, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -13,9 +15,34 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if(@user.update(edit_user_params))
+      redirect_to(account_path)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def edit_user_params
+    params.require(:user).permit(:email, :name, :avatar_url, :role)
+  end
+
+  def ensure_admin
+    if(current_user.role == 'admin')
+      return
+    end
+
+    redirect_to(account_path)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users do
+  resources :users, only: [:new, :create, :edit, :update] do
     resources :goals
   end
 
