b0a4ffadbee6089322d74b9f031d79c864829e17
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-05-31 11:45:01 +0200

    Handling response formats for add goal button in idea card

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -2,8 +2,12 @@ class GoalsController < ApplicationController
   before_action :ensure_authenticated
 
   def create
-    idea = Idea.find(params[:idea_id])
-    current_user.goals << idea
-    redirect_to(idea_path(idea))
+    @idea = Idea.find(params[:idea_id])
+    current_user.goals << @idea
+
+    respond_to do |format|
+      format.html { redirect_to idea_path(@idea) }
+      format.js
+    end
   end
 end
