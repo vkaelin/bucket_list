4d3feda54ba0455481beb5bf7dd656aa08e3e4fe
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-01 10:59:11 +0200

    Front end of uploading photo account

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -21,6 +21,6 @@ class AccountController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :name, :avatar_url)
+    params.require(:user).permit(:email, :name, :avatar_url, :avatar)
   end
 end
