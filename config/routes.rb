ee8f48f1e75d4dba4f3271e70e873b3f7e9bc07d
Kalaneee <33511688+Kalaneee@users.noreply.github.com>
2018-06-01 12:24:40 +0200

    Selecting a language

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,37 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  
+  scope '/:locale' do
+    root to: 'home#index'
 
-  get 'home/index'
+    get 'home/index'
 
-  get 'styles/atoms'
+    get 'styles/atoms'
 
-  get 'styles/molecules'
+    get 'styles/molecules'
 
-  get 'styles/organisms'
+    get 'styles/organisms'
 
-  resources :sessions, only: [:new, :create, :destroy]
+    resources :sessions, only: [:new, :create, :destroy]
 
-  resources :users, only: [:new, :create, :edit, :update] do
-    resources :goals
+    resources :users, only: [:new, :create, :edit, :update] do
+      resources :goals
+    end
+
+    resources :ideas do
+      resources :comments
+      resources :photos
+    end
+
+    get 'account', to: 'account#edit'
+    patch 'account', to: 'account#update'
+    get 'account/ideas'
+    get 'account/goals'
+
+    get 'signup', to: 'users#new'
+
+    get 'login', to: 'sessions#new'
   end
-
-  resources :ideas do
-    resources :comments
-    resources :photos
-  end
-
-  get 'account', to: 'account#edit'
-  patch 'account', to: 'account#update'
-  get 'account/ideas'
-  get 'account/goals'
-
-  get 'signup', to: 'users#new'
-
-  get 'login', to: 'sessions#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
